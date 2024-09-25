2f62248013215996226324ead4797a67e5a294df
Arefevpro <arefevmsk@gmail.com>
2024-09-25 21:41:30 +0300

    commit 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React, {useState} from 'react';
 import {SongsType} from "./data/songsData";
 import allSongs from './data/songsData';
+import {Player} from "./Player";
 
 
 function App() {
-  const [songs, setSongs] = useState<SongsType[]>(allSongs)
-  return (
-      <div className="App">
-
-      </div>
-  );
+    const [songs, setSongs] = useState<SongsType[]>(allSongs)
+    return (
+        <div className="App">
+            <Player/>
+        </div>
+    );
 }
 
 export default App;
